7633662384fbe13d8d6e9ac1ca286c2e233a2399
Mogria <m0gr14@gmail.com>
2018-04-11 20:30:44 +0200

    add script to generate a new post

---
+++ make-post.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+d="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$d"
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: make-post.sh post-title"
+fi
+
+date="$(date --rfc-3339=seconds)"
+date_prefix="$(echo "$date" | cut -d' ' -f1)"
+title="$1"
+title_slug="$(echo "$title" | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9]+/-/g'  | sed -E 's/^-+\|-+$//g' | tr A-Z a-z)"
+
+filename="_posts/$date_prefix-$title_slug.md"
+
+if [ ! -f "$filename" ]; then
+    cat > "$filename" <<NEW_POST
+---
+author: mogli
+layout: post
+title:  $title
+date:   $date
+categories: uftritt
+---
+
+NEW_POST
+else
+    echo "WARN: already exists"
+fi
+
+"$EDITOR" "$filename"
+echo "$filename"
