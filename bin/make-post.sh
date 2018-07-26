9e73b2b7bf4bc9e288631ef5071d1b05e5154ae4
Mogria <m0gr14@gmail.com>
2018-07-26 19:55:47 +0200

    add chraftgwalt show next autmn

--- bin/make-post.sh
+++ bin/make-post.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-d="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+set -e
+
+d="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 cd "$d"
 
 if [ "$#" -lt 1 ]; then
@@ -12,7 +14,7 @@ date_prefix="$(echo "$date" | cut -d' ' -f1)"
 title="$1"
 title_slug="$(echo "$title" | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9]+/-/g'  | sed -E 's/^-+\|-+$//g' | tr A-Z a-z)"
 
-filename="_posts/$date_prefix-$title_slug.md"
+filename="$d/_posts/$date_prefix-$title_slug.md"
 
 if [ ! -f "$filename" ]; then
     cat > "$filename" <<NEW_POST
