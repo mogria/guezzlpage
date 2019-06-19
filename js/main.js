136dc58b35fb9360ff159957eb0fb456a68026c1
Mogria <m0gr14@gmail.com>
2019-06-19 20:28:36 +0200

    remove facebook integration

--- js/main.js
+++ js/main.js
@@ -23,33 +23,11 @@ function show_emails() {
     });
 }
 
-function embed_facebook_sdk() {
-  $.ajaxSetup({ cache: true });
-  $.getScript('//connect.facebook.net/en_US/sdk.js', function(){
-    FB.init({
-      appId: '696265037233127',
-      xfbml: true,
-      version: 'v2.10'
-    });
-    FB.AppEvents.logPageView();
-  });
-}
-
 function htmlencode(v) {
     return $("<div />").html(v).text();
-}
-
-function toggle_asoziali_medie() {
-    var $c = $('.asoziali-medie');
-    $.get({{ site.baseurl | jsonify }} + "/asoziali-medie.html", function(new_content) {
-        $c.html(new_content.replace('@URL@', htmlencode(location.href)));
-        embed_facebook_sdk();
-    });
 }
 
 jQuery(function($) {
     swap_titles(5000);
     show_emails();
-
-    $('#asoziali-medie-aktiviere').click(toggle_asoziali_medie);
 });
